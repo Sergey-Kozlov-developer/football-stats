c0b3fb62f7c36048e0f74112bb16dcb8fe65fdd4
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2026-07-29 14:53:40 +0300

    change ui footbal team card

--- src/widgets/football-teams/ui/FootballTeams.tsx
+++ src/widgets/football-teams/ui/FootballTeams.tsx
@@ -1,18 +1,18 @@
 import TeamCard from "@widgets/football-teams/ui/team-card/TeamCard.tsx";
 import {useGetTeamsQuery} from "@entities/teams/api/teamsApi.ts";
+import styles from "./FootballTeams.module.scss";
+
 
 export const FootballTeams = () => {
 	const {data, isLoading, isError} = useGetTeamsQuery()
 	return (
 		<div>
 			<h1>FootballTeam</h1>
+			<div  className={styles.wrapper}>
 				{data?.map((team) => (
-					<div key={team.id}>
-
-					<TeamCard team={team} />
-					</div>
-
-					))}
+					<TeamCard key={team.id} team={team}/>
+				))}
+			</div>
 		</div>
 	);
 };
--- src/widgets/football-teams/ui/team-card/TeamCard.tsx
+++ src/widgets/football-teams/ui/team-card/TeamCard.tsx
@@ -1,4 +1,5 @@
 import type {ITeam} from "@entities/teams/model/types.ts";
+import styles from "./TeamCard.module.scss";
 
 interface ITeamCardProps {
 	team: ITeam;
@@ -7,10 +8,15 @@ interface ITeamCardProps {
 
 const TeamCard = ({team}: ITeamCardProps) => {
 	return (
-		<div>
-			<h3>{team.name}</h3>
-			<img src={team.logoUrl} alt="Country"/>
-			<p>{team.country.name}</p>
+		<div className={styles.card}>
+			<div className={styles.title}>
+				<img className={styles.flag} src={team.logoUrl} alt="Country"/>
+					<h3 className={styles.name}>{team.name}</h3>
+					<span className={styles.code}>{team.country.code}</span>
+			</div>
+			<button className={styles.button} onClick={() => {}}>
+				Info
+			</button>
 		</div>
 	);
 };
